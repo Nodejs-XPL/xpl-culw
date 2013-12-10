89dbbae0fc2b9cdf6e1ae2687e4e5255ff990071
oeuillot <olivier.oeuillot@gmail.com>
2013-12-10 11:44:38 +0100

    First version

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 
 ## Objective
 
+CULW bridge for XPL layer
 
+## Installation
 
+    $ npm install xpl-culw
+    
 ## Usage
 
+    $ node xpl-culw -i COM4
 
+	For help
 
-## Developing
+    $ node xpl-culw --help
 
-
-
-### Tools
-
-Created with [Nodeclipse v0.5](https://github.com/Nodeclipse/nodeclipse-1)
- ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
--- xpl-culw.js
+++ xpl-culw.js
@@ -1,107 +1,118 @@
 var Xpl = require("xpl-api");
 var program = require('commander');
 var serialport = require("serialport");
-var process = require('process');
 var CulwSerial = require('./lib/culw-serial');
 var os = require('os');
 
-program.option("-s, --xplSource <name>", "Source name for XPL messages")
-		.option("-l, --listSerialPorts", "List known serial ports").option(
-				"-i, --serialPort <path>", "Serial device path").parse(
-				process.argv);
+program.version('0.0.1')
+		.option("-s, --serialPort <path>", "Serial device path");
 
-if (program.listSerialPorts) {
-	console.log("List serial ports:");
-	serialport.list(function(err, ports) {
-		if (err) {
-			console.log("End of list");
+Xpl.fillCommander(program);
 
-			process.exit(0);
-		}
-		ports.forEach(function(port) {
-			console.log("Port name='" + port.comName + "' pnpId='" + port.pnpId
-					+ "' manufacturer='" + port.manufacturer + "'");
-		});
-		console.log("End of list");
+program.command('listSerialPort').description("List serial ports").action(
+		function() {
 
-	});
+			console.log("List serial ports:");
+			serialport.list(function(err, ports) {
+				if (err) {
+					console.log("End of list");
 
-} else {
-
-	var sp = new serialport.SerialPort(program.serialPort, {
-		baudrate : 9600,
-		databits : 8,
-		stopbits : 1,
-		parity : 'none',
-		rtscts : false,
-		parser : serialport.parsers.readline("\n")
-	});
-
-	sp.on("open", function(error) {
-		try {
-			if (error) {
-				console.log("Can not open serial device ", error);
-				process.exit(1);
-				return;
-			}
-			console.log("Serial device '" + program.serialPort + "' opened.");
-
-			var xpl = new Xpl({
-				source : program.xplSource || "culw." + os.hostname(),
-				log: false
-			});
-	
-			xpl.on("error", function(error) {
-				console.log("XPL error", error);
-			});
-
-			xpl.bind(function(error) {
-				if (error) {
-					console.log("Can not open xpl bridge ", error);
-					process.exit(2);
-					return;
+					process.exit(0);
 				}
+				ports.forEach(function(port) {
+					console.log("  Port name='" + port.comName + "' pnpId='"
+							+ port.pnpId + "' manufacturer='"
+							+ port.manufacturer + "'");
+				});
+				console.log("End of list");
 
-				console.log("Xpl bind succeed ");
+			});
+		});
 
-				new CulwSerial(function(data, callback) {
-					//console.log("Write '" + data + "'");
-					sp.write(data, callback);
-					
-				}, function(body, callback) {
-					xpl.sendXplTrig(body, callback);
-					
-				}, {
-				// Configuration
-				}, function(error, culw) {
+program.command('start').description("Start processing CULW datas").action(
+		function() {
+			console.log("Start");
+
+			var sp = new serialport.SerialPort(program.serialPort, {
+				baudrate : 9600,
+				databits : 8,
+				stopbits : 1,
+				parity : 'none',
+				rtscts : false,
+				parser : serialport.parsers.readline("\n")
+			});
+
+			sp.on("open", function(error) {
+				try {
 					if (error) {
-						console.log("Can not initialize CULW engine ", error);
-						process.exit(3);
+						console.log("Can not open serial device '"
+								+ program.serialPort + "'", error);
+						process.exit(1);
 						return;
 					}
+					console.log("Serial device '" + program.serialPort
+							+ "' opened.");
 
-					sp.on('data', function(data) {
-						//console.log('data received: ' + data+"'");
+					if (!program.source) {
+						program.source="culw." + os.hostname();
+					}
+					
+					var xpl = new Xpl(program);
 
-						culw.processSerialData(data);
+					xpl.on("error", function(error) {
+						console.log("XPL error", error);
 					});
 
-					sp.on('close', function() {
-						console.log('close received: ' + data);
+					xpl.bind(function(error) {
+						if (error) {
+							console.log("Can not open xpl bridge ", error);
+							process.exit(2);
+							return;
+						}
 
-						culw.close();
+						console.log("Xpl bind succeed ");
 
-						xpl.close();
+						new CulwSerial(function(data, callback) {
+							// console.log("Write '" + data + "'");
+							sp.write(data, callback);
+
+						}, function(body, callback) {
+							xpl.sendXplTrig(body, callback);
+
+						}, {
+						// Configuration
+						}, function(error, culw) {
+							if (error) {
+								console.log("Can not initialize CULW engine ",
+										error);
+								process.exit(3);
+								return;
+							}
+
+							sp.on('data', function(data) {
+								// console.log('data received: ' + data+"'");
+
+								culw.processSerialData(data);
+							});
+
+							sp.on('close', function() {
+								console.log('close received: ' + data);
+
+								culw.close();
+
+								xpl.close();
+							});
+
+							xpl.on("xpl:xpl-cmnd", function(message) {
+								culw.processXplMessage(message);
+							});
+						});
+
 					});
-
-					xpl.on("message", function(message) {
-						culw.processXplMessage(message);
-					});
-				});
-
+				} catch (x) {
+					console.log(x);
+				}
 			});
-		} catch (x) {
-			console.log(x);
-		}
-	});
-}
+		});
+
+program.parse(process.argv);
