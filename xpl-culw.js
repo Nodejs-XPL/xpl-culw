cc5186ee7234abc387333f2dd45b731b4797322f
oeuillot <olivier.oeuillot@gmail.com>
2015-05-14 12:42:22 +0200

    Add deviceAliases

--- xpl-culw.js
+++ xpl-culw.js
@@ -4,141 +4,161 @@ var serialport = require("serialport");
 var CulwSerial = require('./lib/culw-serial');
 var os = require('os');
 
-commander.version(require("./package.json").version).option(
-    "-s, --serialPort <path>", "Serial device path");
+commander.version(require("./package.json").version).option("-s, --serialPort <path>", "Serial device path");
 
 commander.option("--heapDump", "Enable heap dump (require heapdump)");
+commander.option("--deviceAliases", "Device aliases (path or string)");
 
 Xpl.fillCommander(commander);
 
 commander.command('listSerialPort').description("List serial ports").action(
-    function() {
+		function() {
 
-      console.log("List serial ports:");
-      serialport.list(function(err, ports) {
-        if (err) {
-          console.log("End of list");
+			console.log("List serial ports:");
+			serialport.list(function(err, ports) {
+				if (err) {
+					console.log("End of list");
 
-          process.exit(0);
-        }
-        ports.forEach(function(port) {
-          console.log("  Port name='" + port.comName + "' pnpId='" +
-              port.pnpId + "' manufacturer='" + port.manufacturer + "'");
-        });
-        console.log("End of list");
+					process.exit(0);
+				}
+				ports.forEach(function(port) {
+					console.log("  Port name='" + port.comName + "' pnpId='" + port.pnpId + "' manufacturer='" +
+							port.manufacturer + "'");
+				});
+				console.log("End of list");
 
-      });
-    });
+			});
+		});
 
-commander
-    .command('start')
-    .description("Start processing CULW datas")
-    .action(
-        function() {
-          console.log("Start");
+commander.command('start').description("Start processing CULW datas").action(function() {
+	console.log("Start");
 
-          if (!commander.serialPort) {
-            switch (os.platform()) {
-            case "win32":
-              commander.serialPort = "COM4";
-              break;
-            case "linux":
-              commander.serialPort = "/dev/serial/by-id/usb-busware.de_CUL868-if00";
-              break;
-            }
+	var deviceAliases = commander.deviceAliases;
+	if (deviceAliases) {
+		if (deviceAliases.indexOf('=') >= 0) {
+			var ds = {};
+			commander.deviceAliases = ds;
 
-            console.log("Use default serial port : " + commander.serialPort);
-          }
+			var js = deviceAliases.split(',');
+			for (var i = 0; i < js.length; i++) {
+				var j = js[i].split('=');
+				if (j.length === 2) {
+					ds[j[0].trim()] = j[1].trim();
+				}
+			}
 
-          var sp = new serialport.SerialPort(commander.serialPort, {
-            baudrate : 9600,
-            databits : 8,
-            stopbits : 1,
-            parity : 'none',
-            rtscts : false,
-            parser : serialport.parsers.readline("\n")
-          });
+		} else {
+			commander.deviceAliases = require(deviceAliases);
+		}
+	}
 
-          sp.on("open", function(error) {
-            try {
-              if (error) {
-                console.log("Can not open serial device '" +
-                    commander.serialPort + "'", error);
-                process.exit(1);
-                return;
-              }
-              console.log("Serial device '" + commander.serialPort +
-                  "' opened.");
+	if (!commander.serialPort) {
+		switch (os.platform()) {
+		case "win32":
+			commander.serialPort = "COM4";
+			break;
+		case "linux":
+			commander.serialPort = "/dev/serial/by-id/usb-busware.de_CUL868-if00";
+			break;
+		}
 
-              if (!commander.xplSource) {
-                var hostName = os.hostname();
-                if (hostName.indexOf('.') > 0) {
-                  hostName = hostName.substring(0, hostName.indexOf('.'));
-                }
+		console.log("Use default serial port : " + commander.serialPort);
+	}
 
-                commander.xplSource = "culw." + hostName;
-              }
+	var sp = new serialport.SerialPort(commander.serialPort, {
+		baudrate: 9600,
+		databits: 8,
+		stopbits: 1,
+		parity: 'none',
+		rtscts: false,
+		parser: serialport.parsers.readline("\n")
+	});
 
-              var xpl = new Xpl(commander);
+	sp.on("open", function(error) {
+		try {
+			if (error) {
+				console.log("Can not open serial device '" + commander.serialPort + "'", error);
+				process.exit(1);
+				return;
+			}
+			console.log("Serial device '" + commander.serialPort + "' opened.");
 
-              xpl.on("error", function(error) {
-                console.log("XPL error", error);
-              });
+			if (!commander.xplSource) {
+				var hostName = os.hostname();
+				if (hostName.indexOf('.') > 0) {
+					hostName = hostName.substring(0, hostName.indexOf('.'));
+				}
 
-              xpl.bind(function(error) {
-                if (error) {
-                  console.log("Can not open xpl bridge ", error);
-                  process.exit(2);
-                  return;
-                }
+				commander.xplSource = "culw." + hostName;
+			}
 
-                console.log("Xpl bind succeed ");
+			var xpl = new Xpl(commander);
 
-                new CulwSerial(function(data, callback) {
-                  // console.log("Write '" + data + "'");
-                  sp.write(data, callback);
+			xpl.on("error", function(error) {
+				console.log("XPL error", error);
+			});
 
-                }, function(body, callback) {
-                  xpl.sendXplTrig(body, callback);
+			xpl.bind(function(error) {
+				if (error) {
+					console.log("Can not open xpl bridge ", error);
+					process.exit(2);
+					return;
+				}
 
-                }, {
-                // Configuration
-                }, function(error, culw) {
-                  if (error) {
-                    console.log("Can not initialize CULW engine ", error);
-                    process.exit(3);
-                    return;
-                  }
+				console.log("Xpl bind succeed ");
 
-                  sp.on('data', function(data) {
-                    // console.log('data received: ' + data+"'");
+				new CulwSerial(function(data, callback) {
+					// console.log("Write '" + data + "'");
+					sp.write(data, callback);
 
-                    culw.processSerialData(data);
-                  });
+				}, function(body, callback) {
+					var deviceAliases = commander.deviceAliases;
 
-                  sp.on('close', function() {
-                    console.log('close received: ' + data);
+					if (deviceAliases) {
+						var da = deviceAliases[body.device];
+						if (da) {
+							body.device = da;
+						}
+					}
 
-                    culw.close();
+					xpl.sendXplTrig(body, callback);
 
-                    xpl.close();
-                  });
+				}, commander, function(error, culw) {
+					if (error) {
+						console.log("Can not initialize CULW engine ", error);
+						process.exit(3);
+						return;
+					}
 
-                  xpl.on("xpl:xpl-cmnd", function(message) {
-                    culw.processXplMessage(message);
-                  });
-                });
+					sp.on('data', function(data) {
+						// console.log('data received: ' + data+"'");
 
-              });
-            } catch (x) {
-              console.log(x);
-            }
-          });
-        });
+						culw.processSerialData(data);
+					});
+
+					sp.on('close', function() {
+						console.log('close received: ' + data);
+
+						culw.close();
+
+						xpl.close();
+					});
+
+					xpl.on("xpl:xpl-cmnd", function(message) {
+						culw.processXplMessage(message);
+					});
+				});
+
+			});
+		} catch (x) {
+			console.log(x);
+		}
+	});
+});
 
 commander.parse(process.argv);
 
 if (commander.headDump) {
-  var heapdump = require("heapdump");
-  console.log("***** HEAPDUMP enabled **************");
+	var heapdump = require("heapdump");
+	console.log("***** HEAPDUMP enabled **************");
 }
